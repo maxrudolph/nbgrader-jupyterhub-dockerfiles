13beb6374d51fa5304879533acb7737bd8519f38
Max Rudolph <maxwellr@gmail.com>
2018-12-07 08:57:16 -0800

    bug in add_users.py

--- addusers.py
+++ addusers.py
@@ -30,8 +30,8 @@ for user in userfile:
         uid = fields[3]
         # add the system users
         subprocess.run(['useradd','-m','-s','/bin/bash',username])
-        subproceess.run(['mkdir','/home/%s/.jupyter',username])
-        subprocess.run(['cp','/srv/jupyterhub_config/user_nbgrader_config.py','/home/%s/.jupyter/nbgrader_config.py' % username])'
+        subprocess.run(['mkdir','/home/%s/.jupyter' % username])
+        subprocess.run(['cp','/srv/jupyterhub_config/user_nbgrader_config.py','/home/%s/.jupyter/nbgrader_config.py' % username])
         # restrict permissions for students!
         subprocess.run(['chmod','700','/home/%s' % username])
         # check to see if user is in the nbgrader db.
